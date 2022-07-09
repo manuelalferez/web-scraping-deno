fa0261f6fcc117f3864ce1c5d6ba9f469e43905c
Manuel Alférez <manuelalferezruiz@gmail.com>
2022-07-09 13:25:18 +0200

    selecting the requiered HTML node using query selector

--- scraper.ts
+++ scraper.ts
@@ -1,10 +1,15 @@
-const url = "https://example.com"; 
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+
+const url = "https://example.com";
 
 try {
-  const res = await fetch(url); 
-  const html = await res.text(); 
+  const res = await fetch(url);
+  const html: string = await res.text();
+  const document = new DOMParser().parseFromString(html, "text/html");
 
-  console.log(html);
-} catch(error) {
+  const pageHeader: any = document.querySelector("h1").textContent;
+
+  console.log(pageHeader);
+} catch (error) {
   console.log(error);
 }
